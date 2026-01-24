a7328c34874512a23c6619ba6645c0ccd8c72eec
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-25 05:16:55 +0600

    TechnicalSkill design update

--- components/TechnicalSkill.tsx
+++ components/TechnicalSkill.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import { BiLogoTypescript } from "react-icons/bi";
-import { FaCode, FaLaptopCode, FaReact } from "react-icons/fa";
+import { FaCode, FaGithub, FaLaptopCode, FaReact } from "react-icons/fa";
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { motion } from "motion/react"
 import { IoLogoNodejs } from "react-icons/io5";
-import { SiExpress } from "react-icons/si";
+import { SiExpress, SiFirebase, SiRedux } from "react-icons/si";
 import { DiMongodb } from "react-icons/di";
+import { VscVscode } from "react-icons/vsc";
 
 const TechnicalSkill = () => {
     return (
         <div className=" bg-[#161E2D] flex flex-col justify-center items-center">
-            <div className="min-h-screen max-w-7xl font-roboto">
+            <div className="min-h-screen max-w-6xl font-roboto">
                 <h2 className="text-5xl py-10 text-center text-[#7F22FE] font-bold">Technical Skills</h2>
-                <div className="flex gap-5">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                     <motion.div
                         initial={{
                             y: 0
@@ -35,7 +36,7 @@ const TechnicalSkill = () => {
                                 <p className="text-2xl">Frontend</p>
                             </div>
                         </div>
-                        <div className="flex gap-5 my-5">
+                        <div className="flex flex-wrap gap-4 my-5">
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
@@ -74,7 +75,7 @@ const TechnicalSkill = () => {
                                 <div className="flex items-center text-white bg-[#005661] border-2 border-[#00A7BC] text-4xl p-3 rounded-2xl">
                                     <RiTailwindCssFill />
                                 </div>
-                                <p className="text-[#e6e5e5]">Tailwind CSS</p>
+                                <p className="text-[#e6e5e5]">Tailwind</p>
                             </motion.div>
                         </div>
                     </motion.div>
@@ -99,7 +100,7 @@ const TechnicalSkill = () => {
                                 <p className="text-2xl">Backend</p>
                             </div>
                         </div>
-                        <div className="flex gap-5 my-5">
+                        <div className="flex flex-wrap gap-4 my-5">
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
@@ -160,49 +161,49 @@ const TechnicalSkill = () => {
                                 <div className="bg-[#7F22FE] p-2 rounded-full">
                                     <FaCode className="text-4xl" />
                                 </div>
-                                <p className="text-2xl">Backend</p>
+                                <p className="text-2xl">Technologies & Tools</p>
                             </div>
                         </div>
-                        <div className="flex gap-5 my-5">
+                        <div className="flex flex-wrap gap-4 my-5">
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
                                 }}
                                 className="flex flex-col items-center gap-2">
-                                <div className="flex items-center text-white bg-[#21331c] border-2 border-[#6FA661] text-4xl p-3 rounded-2xl">
-                                    <IoLogoNodejs />
+                                <div className="flex items-center text-white bg-[#072438] border-2 border-[#1E97E8] text-4xl p-3 rounded-2xl">
+                                    <VscVscode />
                                 </div>
-                                <p className="text-[#e6e5e5]">Node.js</p>
+                                <p className="text-[#e6e5e5]">VS Code</p>
                             </motion.div>
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
                                 }}
                                 className="flex flex-col items-center gap-2">
-                                <div className="flex items-center text-white bg-[#3a3a3a] border-2 border-[#eeeeeee8] text-4xl p-3 rounded-2xl">
-                                    <SiExpress />
+                                <div className="flex items-center text-white bg-[#3a3d41] border-2 border-[#1A1E22] text-4xl p-3 rounded-2xl">
+                                    <FaGithub />
                                 </div>
-                                <p className="text-[#e6e5e5]">Express.js</p>
+                                <p className="text-[#e6e5e5]">GitHub</p>
                             </motion.div>
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
                                 }}
                                 className="flex flex-col items-center gap-2">
-                                <div className="flex items-center text-white bg-[#05381a] border-2 border-[#10A54D] text-4xl p-3 rounded-2xl">
-                                    <DiMongodb />
+                                <div className="flex items-center text-white bg-[#502e01] border-2 border-[#F78C00] text-4xl p-3 rounded-2xl">
+                                    <SiFirebase />
                                 </div>
-                                <p className="text-[#e6e5e5]">MongoDB</p>
+                                <p className="text-[#e6e5e5]">Firebase</p>
                             </motion.div>
                             <motion.div
                                 whileHover={{
                                     scale: 1.1
                                 }}
                                 className="flex flex-col items-center gap-2">
-                                <div className="flex items-center text-white bg-[#005661] border-2 border-[#00A7BC] text-4xl p-3 rounded-2xl">
-                                    <FaLaptopCode />
+                                <div className="flex items-center text-white bg-[#25173b] border-2 border-[#7248B5] text-4xl p-3 rounded-2xl">
+                                    <SiRedux />
                                 </div>
-                                <p className="text-[#e6e5e5]">REST</p>
+                                <p className="text-[#e6e5e5]">Redux</p>
                             </motion.div>
                         </div>
                     </motion.div>
