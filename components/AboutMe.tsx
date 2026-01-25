2a504d7c612db985f3c3bfcb2d8ac8267d89dc74
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-25 17:26:58 +0600

    AboutMe section update

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -9,10 +9,10 @@ const AboutMe = () => {
                 <h1 className="text-5xl py-20 text-center text-[#7209b7] font-bold">About Me</h1>
                 <div className="flex flex-col lg:flex-row justify-between">
                     <div className="lg:w-[40%]">
-                        <Image className="w-full lg:h-full rounded-xl" src={"/about/kalidash.jpg"} alt="Kalidash Odekare" width={500} height={300} />
+                        <Image className="w-96 h-96  rounded-xl" src={"/about/kalidash.jpg"} alt="Kalidash Odekare" width={500} height={300} />
                     </div>
                     <div className="lg:w-[50%]">
-                        <p className="text-[#f0efefbb] text-[18px] p-5 lg:p-0">I am a Frontend Developer specializing in React.js, Next.js, and Tailwind CSS, with a strong focus on building user-friendly, responsive, and high-performance web applications. I also have hands-on experience with backend technologies such as Node.js, Express.js, and MongoDB, enabling me to contribute effectively to MERN stack development when required. I enjoy transforming UI/UX designs into clean, scalable, and maintainable code while following best practices. I am continuously learning modern web technologies and improving my problem-solving skills. I work well in collaborative team environments, value clean code and clear communication, and aim to grow into a proficient Full Stack Developer within the next five years.</p>
+                        <p className="text-[#f0efefbb] leading-7 text-[17px] p-5 lg:p-0">I am a Frontend Developer specializing in React.js, Next.js, and Tailwind CSS, with a strong focus on building user-friendly, responsive, and high-performance web applications. I also have hands-on experience with backend technologies such as Node.js, Express.js, and MongoDB, enabling me to contribute effectively to MERN stack development when required. I enjoy transforming UI/UX designs into clean, scalable, and maintainable code while following best practices. I am continuously learning modern web technologies and improving my problem-solving skills. I work well in collaborative team environments, value clean code and clear communication, and aim to grow into a proficient Full Stack Developer within the next five years.</p>
                     </div>
                 </div>
             </div>
