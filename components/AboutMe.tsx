d9cfd083ef0584d381ac8a033cd9db7c3c18d7e3
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-01-31 21:05:46 +0600

    AboutMe section dark mode update

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -10,23 +10,23 @@ const AboutMe = () => {
     return (
         <div id="about" className="-mt-px relative bg-[#0D1317] overflow-hidden dark:bg-white flex flex-col ">
             <div className="min-h-screen lg:max-w-6xl m-auto pb-10 font-rubik mt-24">
-                <div className="text-white flex flex-col lg:flex-row items-center gap-10 mx-5">
-                    <div className="lg:w-[60%] space-y-3">
+                <div className="text-white flex flex-col-reverse lg:flex-row items-center gap-10 mx-5">
+                    <div className="lg:w-[60%] space-y-3 z-20">
                         <div className="flex items-center gap-2 text-xl">
                             <p className="w-14 border-2 border-[#6532FF]" />
-                            <p>About me</p>
+                            <p className="dark:text-[#000000bb]">About me</p>
                             <p className="w-14 border-2 border-[#6532FF]" />
                         </div>
-                        <h1 className="text-4xl">Frontend Developer</h1>
-                        <p className="text-[#dad9d9bb] text-[17px]">I am a Frontend Developer specializing in React.js, Next.js, and Tailwind CSS, with a strong focus on building user-friendly, responsive, and high-performance web applications. I also have hands-on experience with backend technologies such as Node.js, Express.js, and MongoDB, enabling me to contribute effectively to MERN stack development when required. I enjoy transforming UI/UX designs into clean, scalable, and maintainable code while following best practices. I am continuously learning modern web technologies and improving my problem-solving skills. I work well in collaborative team environments, value clean code and clear communication, and aim to grow into a proficient Full Stack Developer within the next five years.</p>
+                        <h1 className="text-4xl dark:text-[#6532FF]">Frontend Developer</h1>
+                        <p className="text-[#dad9d9bb] text-[17px] dark:text-[#000000bb]">I am a Frontend Developer specializing in React.js, Next.js, and Tailwind CSS, with a strong focus on building user-friendly, responsive, and high-performance web applications. I also have hands-on experience with backend technologies such as Node.js, Express.js, and MongoDB, enabling me to contribute effectively to MERN stack development when required. I enjoy transforming UI/UX designs into clean, scalable, and maintainable code while following best practices. I am continuously learning modern web technologies and improving my problem-solving skills. I work well in collaborative team environments, value clean code and clear communication, and aim to grow into a proficient Full Stack Developer within the next five years.</p>
                         <div className="flex flex-wrap gap-5 border border-[#a0a0a0bb] p-5 rounded-4xl">
                             <div className="flex items-center gap-2">
                                 <div className="bg-[#6532FF] text-2xl p-2 rounded-full">
                                     <FaPhoneAlt />
                                 </div>
                                 <div>
-                                    <p className="font-bold">Phone:</p>
-                                    <p className="text-[#ebebebbb] text-[16px]">+8801728659562</p>
+                                    <p className="font-bold dark:text-[#000000bb]">Phone:</p>
+                                    <p className="text-[#ebebebbb] dark:text-[#000000bb] text-[16px]">+8801728659562</p>
                                 </div>
                             </div>
                             <div className="flex items-center gap-2">
@@ -34,8 +34,8 @@ const AboutMe = () => {
                                     <MdEmail />
                                 </div>
                                 <div>
-                                    <p className="font-bold">Email:</p>
-                                    <p className="text-[#ebebebbb] text-[16px]">kalidashodekare14@gmail.com</p>
+                                    <p className="font-bold dark:text-[#000000bb]">Email:</p>
+                                    <p className="text-[#ebebebbb] text-[16px] dark:text-[#000000bb]">kalidashodekare14@gmail.com</p>
                                 </div>
                             </div>
                             <div className="flex items-center gap-2">
@@ -43,13 +43,13 @@ const AboutMe = () => {
                                     <FaPhoneAlt />
                                 </div>
                                 <div>
-                                    <p className="font-bold">Address:</p>
-                                    <p className="text-[#ebebebbb] text-[16px]">Birgonj, Dinajpur</p>
+                                    <p className="font-bold dark:text-[#000000bb]">Address:</p>
+                                    <p className="text-[#ebebebbb] text-[16px] dark:text-[#000000bb]">Birgonj, Dinajpur</p>
                                 </div>
                             </div>
                             <div className="flex items-center gap-2">
                                 <div>
-                                    <p className="font-bold">Social:</p>
+                                    <p className="font-bold dark:text-[#000000bb]">Social:</p>
                                     <div className='flex items-center gap-4'>
                                         <Link href={"https://www.linkedin.com/in/kalidash-odekare-webdev"} target='_blank'>
                                             <motion.div
